864339672858f809084122481aa3cfe17edd32d1
psyco1751 <mdhabibhasanornob@trentu.ca>
2024-12-02 17:01:56 -0500

    commit 4
    added file reader for reading transaction.txt

--- Transaction.java
+++ Transaction.java
@@ -18,14 +18,14 @@ public class Transaction {
 	        return instance;
 	    }
 	    public void saveTransaction(String transactionDetails) {
-	        try (BufferedWriter writer = new BufferedWriter(new FileWiter("Transactions.txt", true))) {
+	        try (BufferedWriter writer = new BufferedWriter(new FileWriter("Transactions.txt", true))) {
 	            writer.write(transactionDetails);
 	            writer.newLine();
 	        } catch (IOException e) {
 	            System.out.println("Error saving transaction: " + e.getMessage());}
 	    }
 	    public void displayTransactionHistory() {
-	        try (BufferedReader reader = new BufferedReader(new FileReader("Transactions.txt"))) {
+	        try (BufferedReader reader = new BufferedReader(new Fileeader("Transactions.txt"))) {
 	            String line;
 	            System.out.println("Transaction History:");
 	            while ((line = reader.readLine()) != null) {
